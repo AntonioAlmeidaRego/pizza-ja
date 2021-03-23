bf5f814c487efbf746c60ce86fcef3ca32da5388
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2021-03-23 16:50:12 -0300

    Primeiro Commit

---
+++ pizza-ja/src/main/java/br/com/pizza/PizzaJaApplication.java
@@ -0,0 +1,13 @@
+package br.com.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaJaApplication.class, args);
+	}
+
+}
---
+++ pizza-ja/src/test/java/br/com/pizza/PizzaJaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
